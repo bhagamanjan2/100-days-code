6f76f101e4451dadc3890350563957c05cbc47c3
bhagam anjan <Bhagam.21618@stu.upes.ac.in>
2025-09-04 23:37:32 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    
+    printf("Enter two numbers: ");
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    
+    printf("sum: %d\n",num1+num2);
+    printf("diff: %d\n",num1-num2);
+    printf("prod: %d\n",num1*num2);
+    printf("quot: %d\n",num1/num2);  
+    return 0;
+}
